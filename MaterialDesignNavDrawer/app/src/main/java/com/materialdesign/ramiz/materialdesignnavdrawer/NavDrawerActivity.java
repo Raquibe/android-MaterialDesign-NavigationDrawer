d8cfc03ee2724652dcda913fa2170b8143694d59
Ramiz Raja <informramiz@gmail.com>
2017-07-09 12:33:03 +0500

    refactor: Extract toolbar to action bar into a method

--- MaterialDesignNavDrawer/app/src/main/java/com/materialdesign/ramiz/materialdesignnavdrawer/NavDrawerActivity.java
+++ MaterialDesignNavDrawer/app/src/main/java/com/materialdesign/ramiz/materialdesignnavdrawer/NavDrawerActivity.java
@@ -10,9 +10,11 @@ public class NavDrawerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav_drawer);
+        setupToolbar();
 
-        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    private void setupToolbar() {
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
     }
