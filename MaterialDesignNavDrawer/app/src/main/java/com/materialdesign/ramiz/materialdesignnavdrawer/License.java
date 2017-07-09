8f193bd2a7136b0fd527b322872b94ba3aa9956d
Ramiz Raja <informramiz@gmail.com>
2017-07-09 19:39:23 +0500

    feat: License class to indicate if app is licensed

---
+++ MaterialDesignNavDrawer/app/src/main/java/com/materialdesign/ramiz/materialdesignnavdrawer/License.java
@@ -0,0 +1,11 @@
+package com.materialdesign.ramiz.materialdesignnavdrawer;
+
+/**
+ * Created by ramiz on 7/9/17.
+ */
+
+public class License {
+    public static boolean isUnlocked() {
+        return true;
+    }
+}
