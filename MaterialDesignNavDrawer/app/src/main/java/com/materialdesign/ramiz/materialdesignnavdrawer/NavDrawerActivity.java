6cbd392f2843e8214255190cf073fc67d9f7a280
Ramiz Raja <informramiz@gmail.com>
2017-07-09 12:41:59 +0500

    feat: Add NavigationView and DrawerLayout listeners

--- MaterialDesignNavDrawer/app/src/main/java/com/materialdesign/ramiz/materialdesignnavdrawer/NavDrawerActivity.java
+++ MaterialDesignNavDrawer/app/src/main/java/com/materialdesign/ramiz/materialdesignnavdrawer/NavDrawerActivity.java
@@ -1,16 +1,74 @@
 package com.materialdesign.ramiz.materialdesignnavdrawer;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+import static com.materialdesign.ramiz.materialdesignnavdrawer.R.id.toolbar;
 
 public class NavDrawerActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+    private Toolbar mToolbar;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav_drawer);
         setupToolbar();
+
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mNavigationView = (NavigationView)findViewById(R.id.navigation_view);
+
+        //assign listeners to all the views we are interested in
+        setListeners();
+    }
+
+    private void setListeners() {
+        //add listener to navigation view
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setCheckable(true);
+
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+        //initialize ActoinBarDrawerToggleListener to listen
+        //for navigation drawer open/close events. It also handles
+        //opening nav drawer on hamburger icon click
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.drawer_open,
+                R.string.drawer_close) {
+            //called when nav drawer is completely closed
+            //and settled
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+            //called when nav drawer is completely open
+            //and settled
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        //add listener to navigation drawer, to be called
+        //when drawer opens or closes
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+    }
 
     private void setupToolbar() {
         mToolbar = (Toolbar)findViewById(R.id.toolbar);
