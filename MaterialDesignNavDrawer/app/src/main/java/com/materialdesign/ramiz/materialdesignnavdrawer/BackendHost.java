9dc58befa78166af61b571752f712534a6cf8630
Ramiz Raja <informramiz@gmail.com>
2017-07-15 21:38:56 +0500

    feat: Add NavDrawerHeaderView's BackendHost data model

---
+++ MaterialDesignNavDrawer/app/src/main/java/com/materialdesign/ramiz/materialdesignnavdrawer/BackendHost.java
@@ -0,0 +1,12 @@
+package com.materialdesign.ramiz.materialdesignnavdrawer;
+
+public class BackendHost {
+    public String name;
+    public String client;
+    public String address;
+    public String mac;
+    public String wan;
+    public String password;
+    public boolean promptPassword;
+    public boolean promptUsername;
+}
