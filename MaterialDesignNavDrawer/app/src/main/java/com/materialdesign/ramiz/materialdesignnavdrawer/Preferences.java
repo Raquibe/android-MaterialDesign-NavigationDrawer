aecf4b84a31bdfedf9ea093c8276f2408b25ca90
Ramiz Raja <informramiz@gmail.com>
2017-07-15 22:17:48 +0500

    feat: Add preferences class

---
+++ MaterialDesignNavDrawer/app/src/main/java/com/materialdesign/ramiz/materialdesignnavdrawer/Preferences.java
@@ -0,0 +1,14 @@
+package com.materialdesign.ramiz.materialdesignnavdrawer;
+
+/**
+ * Created by ramiz on 7/15/17.
+ */
+
+//Preferences Wrapper
+public class Preferences {
+    public static class Clients {
+        public static final String TCP = "tcp";
+        public static final String BLUETOOTH = "bt";
+    }
+}
+
