ed722f8b127b2a8381c4bca1808d5762a6c50c20
Ramiz Raja <informramiz@gmail.com>
2017-07-15 21:39:31 +0500

    feat: Prepare NavDrawerHeaderView click listener logic

--- MaterialDesignNavDrawer/app/src/main/java/com/materialdesign/ramiz/materialdesignnavdrawer/NavDrawerHeaderView.java
+++ MaterialDesignNavDrawer/app/src/main/java/com/materialdesign/ramiz/materialdesignnavdrawer/NavDrawerHeaderView.java
@@ -5,9 +5,11 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * This is a custom view for Navigation Drawer header view. It displays a list of
@@ -16,10 +18,13 @@ import android.widget.TextView;
  * Created by ramiz on 7/15/17.
  */
 
-public class NavDrawerHeaderView extends LinearLayout {
+public class NavDrawerHeaderView extends LinearLayout implements View.OnClickListener {
     private ImageView serverIconImageView;
     private TextView serverNameTextView;
     private ImageView downIconImageView;
+
+    @Nullable
+    private OnItemSelectedListener onItemSelectedListener = null;
 
     public NavDrawerHeaderView(Context context) {
         super(context);
@@ -44,5 +49,33 @@ public class NavDrawerHeaderView extends LinearLayout {
         serverIconImageView = findViewById(R.id.image_view_server_icon);
         serverNameTextView = findViewById(R.id.text_view_server_name);
         downIconImageView = findViewById(R.id.image_view_icon_down);
+
+        super.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(getContext(), "Header clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    //we don't anyone else to listen to this event because
+    // we provide a custom OnItemSelectedListener for outside world
+    // so
+    //we have overridden it with empty body
+    @Override
+    public void setOnClickListener(@Nullable OnClickListener l) {
+
+    }
+
+    /**
+     * Notifies the listener when a server is selected
+     * @param onItemSelectedListener
+     */
+    public void setOnItemSelectedListener(@Nullable OnItemSelectedListener onItemSelectedListener) {
+        this.onItemSelectedListener = onItemSelectedListener;
+    }
+
+    public interface OnItemSelectedListener{
+        void onServerSelected();
     }
 }
